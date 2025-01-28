881c968d0b4c9559622af7604206cd1368745b9f
SiriAkunoori <siriakunoori@gmail.com>
2025-01-28 21:22:03 +0530

    Add files via upload

---
+++ Mathpower.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Mathpower{
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter the base");
+        double base=scanner.nextDouble();//double is the data type
+        System.out.println("enter the power");
+        double power=scanner.nextDouble();
+        double result=Math.pow(base,power);
+        System.out.println(result);
+        scanner.close();
+    }    
+}
+    
+
